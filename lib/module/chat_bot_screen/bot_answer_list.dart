55abc0accd212a03464e9b2d9abf8db078b909cc
Şenay Keskin <102466144+senaykeskin@users.noreply.github.com>
2025-04-25 15:04:19 +0300

    chat bot screen was finished. (#14)

---
+++ lib/module/chat_bot_screen/bot_answer_list.dart
@@ -0,0 +1,12 @@
+final Map<String, String> botAnswerList = {
+  "Diyet listesi": "25 yaşında, 165 cm ve 55 kg için örnek diyet:"
+      "\n • Kahvaltı: Yulaf + muz"
+      "\n • Öğle: Tavuk + sebze"
+      "\n • Akşam: Sebzeli omlet",
+  "Kilo alma/verme":
+      "Sağlıklı kilo değişimi için günlük kalori takibi yapabilirsin."
+          "\n • Kilo almak: Kalori fazlası"
+          "\n • Kilo vermek: Kalori açığı",
+  "Spor saatleri":
+      "En verimli spor saatleri sabah 7-10 ya da akşam 17-19 arasıdır. Ama en önemlisi düzenli olmasıdır 💪"
+};
